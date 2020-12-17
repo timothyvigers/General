d486d2b252dacb7483eef76d0f713d8f646caf42
timothyvigers <timothy.vigers@cuanschutz.edu>
2020-12-16 20:25:41 -0700

    Update make_all_github.sh

--- Scripts/make_all_github.sh
+++ Scripts/make_all_github.sh
@@ -6,3 +6,15 @@ make
 cd /Users/timvigers/GitHub/plink-ng/2.0/build_dynamic
 git pull
 make
+
+cd /Users/timvigers/GitHub/htslib
+git pull
+make
+
+cd /Users/timvigers/GitHub/samtools
+git pull
+make
+
+cd /Users/timvigers/GitHub/bcftools
+git pull
+make
