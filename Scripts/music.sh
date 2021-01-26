aacebe34b9ce7decf67cf98e99eabc2d25b6e05b
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-01-26 08:35:05 -0700

    Update music.sh

--- Scripts/music.sh
+++ Scripts/music.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd Music
+cd /home/tim/.local/share/Cryptomator/mnt/Vault/Music
 find . -type f ! -name "*.flac" ! -name "*.mp3" -delete
+find . -type d -empty -delete
+cd ~
 rsync -auv Music/ /media/tim/SD_CARD/MUSIC/ --delete
