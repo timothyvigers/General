8f0b3ad48d2ee37c12a4ea0675205b8ca7800c97
Tim Vigers <timothy.vigers@cuanschutz.edu>
2021-07-10 09:36:34 -0600

    delete no longer used scripts

--- Scripts/sync_flac.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo mount -t drvfs F: /mnt/f # Mount SD drive
-sudo rsync -auv /mnt/c/Users/timbv/Dropbox/Music/ /mnt/f/MUSIC/ # Sync Dropbox to player
-# sudo rsync -auv /mnt/c/Users/timbv/Dropbox/Music/ /mnt/f/MUSIC/ # Player to Dropbox
